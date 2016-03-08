50daae58a84070ffba04e23b10de373a4c1eeb95
Nick-Haener <nhaener@uw.edu>
2016-03-08 15:25:51 -0800

    yes

---
+++ server_data_mp.R
@@ -0,0 +1,22 @@
+#############################################################
+#############################################################
+## This file will be used to manipulate our data sets and 
+## retrieve relevant information.
+##
+## File name convention:
+##
+## See google doc and/or issues for what needs to be done.
+##
+#############################################################
+#############################################################
+
+########################## DATA Manipulation ######################################
+###################################################################################
+## Set working directory
+setwd("/Users/Nick/info498f/INFOFinalProject")
+
+## Source formatted data
+source("data_format.R")
+
+## Library 
+library(dplyr)
