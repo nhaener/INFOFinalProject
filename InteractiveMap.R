199f17fd56fc7df93bccd06142bec79d1b4a5fa5
Nick-Haener <nhaener@uw.edu>
2016-03-08 21:52:09 -0800

    updated interactive map

--- InteractiveMap.R
+++ InteractiveMap.R
@@ -1,5 +1,5 @@
 library(plotly)
-library(dyplyr)
+library(dplyr)
 
 ## Set working directory
 setwd("/Users/Nick/info498f/INFOFinalProject")
@@ -7,7 +7,7 @@ setwd("/Users/Nick/info498f/INFOFinalProject")
 ## Source formatted data
 source("data_format.R")
 
-data <- OB %>% select(1:3, contains('2004')) %>% 
+data <- OB %>% select(1:3, contains('2004'), contains("Lat"), contains("Long")) %>% 
   filter(OB$State == "Washington")
 
 #create interactive global map
@@ -26,14 +26,34 @@ g <- list(
 #create plot
 # Create graph with varrying marker sizes based on percentage obese
 plot_ly(data, 
-        lon = long, 
-        lat = lat, 
+        lon = Long, 
+        lat = Lat, 
         text = paste(data$County, ', ', data$State, 
                      '<br><b>Percentage Obese:</b>', data$percent_2004),
         hoverinfo = 'text',
-        marker = list(size = data$percent_2004 , opacity = .4, color = 'red'),
+        marker = list(size = data$percent_2004 / 4 , opacity = .4, color = 'red'),
         type = 'scattergeo', 
         locationmode = 'USA-states'
 ) %>%
   layout(title = '2004 Crowdsourced Obesity Statistics', geo = g)
 
+
+
+################################# CHOROPLETH ##########################################
+data$hover <- with(data, paste(State, '<br>', "Beef", beef, "Dairy", dairy, "<br>",
+                           "Fruits", total.fruits, "Veggies", total.veggies,
+                           "<br>", "Wheat", wheat, "Corn", corn))
+# give state boundaries a white border
+l <- list(color = toRGB("white"), width = 2)
+# specify some map projection/options
+g <- list(
+  scope = 'usa',
+  projection = list(type = 'albers usa'),
+  showlakes = TRUE,
+  lakecolor = toRGB('white')
+)
+
+plot_ly(df, z = total.exports, text = hover, locations = code, type = 'choropleth',
+        locationmode = 'USA-states', color = total.exports, colors = 'Purples',
+        marker = list(line = l), colorbar = list(title = "Millions USD")) %>%
+  layout(title = '2011 US Agriculture Exports by State<br>(Hover for breakdown)', geo = g)
