839db1c94cec68ff062d9e088698146f6bf096b5
spirrochet <ckites@uw.edu>
2016-03-10 01:04:02 -0800

    interactive map to main page

--- server.R
+++ server.R
@@ -15,8 +15,12 @@ library(shiny)
 library(plotly)
 library(dplyr)
 library(choroplethr)
+library(choroplethrMaps)
 
-
+#############################################################
+# Load manipulated data
+#source("server_data_mp.R")
+source("PAGE_Overview_data_mp.R")
 
 #############################################################
 # Read in data
@@ -24,16 +28,27 @@ OB <- read.csv("data/OB.csv")
 OB_S <- read.csv("data/OB_S.csv")
 AC <- read.csv("data/AC.csv")
 AC_S <- read.csv("data/AC_S.csv")
-  
+
 
 # Server
 shinyServer(function(input, output) {
   
   #############################################################
   # Output for Overview Page 
-  #output$OB_St_level <- renderPlotly({
-    
-   #})
+  
+  # function to coerce setup df
+  
+  output$Overview_OB_map <- renderPlot({
+    df <- OB %>% select(1:3, contains("number"), contains("percent"), contains("Lat"), contains("Long"))
+    colnames(df)[2] <- "region"
+    df <- df %>% select(1:3, contains(paste0(input$Overview_OB_map_select_year)), contains("Lat"), contains("Long"))
+    colnames(df)[5] <- "value"
+    if(input$Overview_OB_map_select_state != "National") {
+      county_choropleth(df, state_zoom = c(tolower(input$Overview_OB_map_select_state)))
+    } else {
+      county_choropleth(df)
+    }
+  })
   
   #############################################################
   # Output for Obesity & Activity
--- ui.R
+++ ui.R
@@ -9,16 +9,36 @@
 #############################################################
 #############################################################
 library(shiny)
+library(plotly)
+library(dplyr)
+library(choroplethr)
+library(choroplethrMaps)
+
+
+#############################################################
+# Load manipulated data
+#source("server_data_mp.R")
+source("PAGE_Overview_data_mp.R")
+
 
 # Define UI
 shinyUI(fluidPage(
   
   navbarPage("US Obesity",
-             tabPanel("Overview"#,
-                    
-                      #plotlyOutput("OB_St_level"),
-                      #sliderInput("OB_st_level_year")
+             tabPanel("Overview",
                       
+                      sidebarLayout(
+                        sidebarPanel( "View Settings:",
+                            selectInput("Overview_OB_map_select_year",
+                                        choices = c(2004:2012),
+                                        label = "Select year"),
+                          selectInput("Overview_OB_map_select_state",
+                                      choices = c("National", choices_state()),
+                                      label = "Select focus (National or State)")
+                        ),
+                        mainPanel("",
+                          plotOutput("Overview_OB_map")
+                        ))
               ),
              tabPanel("Obesity & Activity",
                       sidebarLayout(
